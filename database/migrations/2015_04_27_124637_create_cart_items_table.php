b5293e5d0ac7145267ff39098c9f5334e435c137
Scodia <ahmetalpaslan@hotmail.com>
2015-04-27 15:51:00 +0300

    add cart Item table

---
+++ database/migrations/2015_04_27_124637_create_cart_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCartItemsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('cart_items', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('user_ID');
+			$table->integer('warehouse_product_ID');
+			$table->integer('qunatity');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('cart_items');
+	}
+
+}
